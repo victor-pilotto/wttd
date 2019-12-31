fa3bf361f7c5ebfa13996e81191b90b528936b5e
Victor Pilotto <vcpilotto@gmail.com>
2019-12-31 10:37:50 -0300

    Thank you page

---
+++ eventex/subscriptions/tests/test_view_detail.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionDetailGet(TestCase):
+    def setUp(self) -> None:
+        self.obj = Subscription.objects.create(
+            name='Victor Pilotto',
+            cpf='12345678901',
+            email='teste@teste.com.br',
+            phone='199999999'
+        )
+        self.response = self.client.get('/inscricao/{}/'.format(self.obj.pk))
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'subscriptions/subscription_detail.html')
+
+    def test_context(self):
+        subscription = self.response.context['subscription']
+        self.assertIsInstance(subscription, Subscription)
+
+    def test_html(self):
+        contents = (self.obj.name, self.obj.cpf,
+                    self.obj.email, self.obj.phone)
+
+        with self.subTest():
+            for expected in contents:
+                self.assertContains(self.response, expected)
+
+
+class SubscriptionDetailNotFound(TestCase):
+    def setUp(self) -> None:
+        self.response = self.client.get('/inscricao/0/')
+
+    def test_not_found(self):
+        self.assertEqual(404, self.response.status_code)
--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,5 +1,6 @@
 from django.core import mail
 from django.test import TestCase
+
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
@@ -45,8 +46,8 @@ class SubscribePostValid(TestCase):
         self.response = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.response.status_code)
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.response, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -76,12 +77,3 @@ class SubscribePostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Victor Pilotto', cpf='12345678901',
-                    email='teste@teste.com.br', phone='19-98888-3333')
-
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscricão realizada com sucesso!')
