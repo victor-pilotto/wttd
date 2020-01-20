b4cf543a8ca103fbd2a9922d76a5a1e701ef9bf8
Victor Pilotto <vcpilotto@gmail.com>
2020-01-20 18:27:39 -0300

    Simplificar com modelForm

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -27,7 +27,7 @@ def create(request):
         return render(request, 'subscriptions/subscription_form.html',
                       {'form': form})
 
-    subscription = Subscription.objects.create(**form.cleaned_data)
+    subscription = form.save()
 
     # Send email
     _send_email('Confirmacão de inscricão',
