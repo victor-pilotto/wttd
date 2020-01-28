fb301ac9982a35fd5171e1acc96775659117eb53
Victor Pilotto <vcpilotto@gmail.com>
2020-01-28 19:48:38 -0300

    Courses

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
 
-from eventex.core.models import Talk, Speaker
+from eventex.core.models import Talk, Speaker, Course
 
 
 class TalkListGet(TestCase):
@@ -10,6 +10,8 @@ class TalkListGet(TestCase):
                             description='Descricão da palestra')
         t2 = Talk.objects.create(title='Título da Palestra', start='13:00',
                             description='Descricão da palestra')
+        c1 = Course.objects.create(title='Título do Curso', start='09:00',
+                            description='Descricão do curso', slots=20)
 
         speaker = Speaker.objects.create(name='Henrique Bastos',
                                          slug='henrique-bastos',
@@ -17,6 +19,7 @@ class TalkListGet(TestCase):
 
         t1.speakers.add(speaker)
         t2.speakers.add(speaker)
+        c1.speakers.add(speaker)
 
         self.response = self.client.get(r('talk_list'))
 
@@ -31,9 +34,12 @@ class TalkListGet(TestCase):
             (2, 'Título da Palestra'),
             (1, '10:00'),
             (1, '13:00'),
-            (2, '/palestrantes/henrique-bastos'),
-            (2, 'Henrique Bastos'),
+            (3, '/palestrantes/henrique-bastos'),
+            (3, 'Henrique Bastos'),
             (2, 'Descricão da palestra'),
+            (1, 'Título do Curso'),
+            (1, '09:00'),
+            (1, 'Descricão do curso')
         ]
 
         for count, expected in contents:
@@ -41,7 +47,7 @@ class TalkListGet(TestCase):
                 self.assertContains(self.response, expected, count)
 
     def test_contexts(self):
-        variables = ['morning_talks', 'afternoon_talks']
+        variables = ['morning_talks', 'afternoon_talks', 'courses']
 
         for key in variables:
             with self.subTest():
